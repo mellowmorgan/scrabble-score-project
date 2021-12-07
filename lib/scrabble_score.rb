866cc0b4ea4e2c5e06854c6dbf571536ca5d4609
mellowmorgan <morgan.waites@gmail.com>
2021-12-07 14:22:26 -0800

    file path checked working
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>
    Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>
    Co-Authored-By: Chase Stokes <chasehstokes@gmail.com>

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,38 +1,51 @@
 require('pry')
 
+
+
 class ScrabbleScore
-  def initialize(word)
-    @word = word
+  def initialize(words)
+    @words = words
   end
-  def word
-    @word
+  def words
+    @words
   end
 
   def get_score
-    score=0
-    arr=@word.split("")
-    arr.each do |letter|
-      case letter
-      when 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'
-        score+=1
-      when 'd', 'g'
-        score+=2
-      when 'b', 'c', 'm', 'p'
-        score+=3
-      when 'f', 'h', 'v', 'w', 'y'
-        score+=4
-      when 'k'
-        score+=5
-      when 'j', 'x'
-        score+=8
-      when 'q', 'z'
-        score+=10            
-      else
-        score+=0
+    big_score=0
+    big_arr=@words.split(" ")
+    big_arr.each do |word|
+      arr=word.split("")
+      score=0
+      arr.each do |letter|
+        case letter.downcase
+        when 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'
+          score+=1 
+        when 'd', 'g'
+          score+=2 
+        when 'b', 'c', 'm', 'p'
+          score+=3
+        when 'f', 'h', 'v', 'w', 'y'
+          score+=4
+        when 'k'
+          score+=5
+        when 'j', 'x'
+          score+=8
+        when 'q', 'z'
+          score+=10            
+        else
+          score+=0
+        end
       end
+      big_score+=score
+    
     end
-    score
+    big_score
   end
 end
 
-# word_test_1 = ScrabbleScore.new("a")
+file = File.open('./../moby10b.txt')
+data = file.read
+file.close
+test = ScrabbleScore.new(data)
+puts test.get_score
+
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -1,14 +1,16 @@
 require('rspec')
 require('scrabble_score.rb')
 
+
 describe("ScrabbleScore#get_score") do 
   word_test_1 = ScrabbleScore.new("a")
   word_test_3 = ScrabbleScore.new("at")
   word_test_4 = ScrabbleScore.new("aei123")
   word_test_5 = ScrabbleScore.new("giraffe")
-  it("should return a for variable word_test_1") do
-    expect(word_test_1.word).to(eq("a"))
-  end
+  word_test_6 = ScrabbleScore.new("i like dogs")
+  # it("should return a for variable word_test_1") do
+  #   expect(word_test_1.word).to(eq("a"))
+  # end
   it("should return score of 1 for all 1-point letters") do
     expect(word_test_1.get_score).to(eq(1))
   end
@@ -20,6 +22,9 @@ describe("ScrabbleScore#get_score") do
   end
   it("should return score of 14 for giraffe") do
     expect(word_test_5.get_score).to(eq(14))
+  end
+  it("should return score of  for 'i like dogs'") do
+    expect(word_test_6.get_score).to(eq(15))
   end
 end
 
